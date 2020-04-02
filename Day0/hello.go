c2e6a8c6004cc6f583c2293883b2d6f9c318db86
canro91 <canro91@hotmail.com>
2020-04-01 23:03:06 -0500

    Polyglot Hello with if/else

--- Day0/hello.go
+++ Day0/hello.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-    return "Hello, " + name + "!"
+const spanish = "Spanish"
+const french = "French"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+    if name == "" {
+        name = "world"
+    }
+
+    prefix := englishHelloPrefix
+    if language == spanish {
+        prefix = spanishHelloPrefix
+    } else if language == french {
+        prefix = frenchHelloPrefix
+    }
+
+    return prefix + name + "!"
 }
 
 func main(){
-    fmt.Println(Hello("world"))
+    fmt.Println(Hello("world", ""))
 }
--- Day0/hello_test.go
+++ Day0/hello_test.go
@@ -21,13 +21,51 @@ import "testing"
 
 // Unused imports give a compilation error
 
-func TestHello(t *testing.T) {
-    expected := "Hello, Alice!"
-    actual := Hello("Alice")
+// %q encloses a string inside quotes
 
-    if expected != actual {
-        // %q encloses a string inside quotes
-        t.Errorf("Expected %q but was %q", expected, actual)
+// There is no method overloading
+
+// https://stackoverflow.com/a/17891297
+// := declaration + assignment
+// = assignment only
+
+func TestHello(t *testing.T) {
+
+    assertAreEqual := func(t *testing.T, expected, actual string){
+        t.Helper()
+
+        if expected != actual {
+            t.Errorf("Expected %q but was %q", expected, actual)
+        }
     }
+
+    t.Run("Hello_Name_ReturnsHelloWithName", func(t *testing.T){
+        expected := "Hello, Alice!"
+        actual := Hello("Alice", "")
+
+        assertAreEqual(t, expected, actual)
+    })
+
+    t.Run("Hello_EmptyString_ReturnsHelloWorld", func(t *testing.T){
+        expected := "Hello, world!"
+        actual := Hello("", "")
+
+        assertAreEqual(t, expected, actual)
+    })
+
+    t.Run("Hello_SpanishAndName_ReturnsHelloInSpanish", func(t *testing.T){
+        expected := "Hola, Alice!"
+        actual := Hello("Alice", "Spanish")
+
+        assertAreEqual(t, expected, actual)
+    })
+
+    t.Run("Hello_FrenchAndName_ReturnsHelloInSpanish", func(t *testing.T){
+        expected := "Bonjour, Alice!"
+        actual := Hello("Alice", "French")
+
+        assertAreEqual(t, expected, actual)
+    })
 }
+
 
