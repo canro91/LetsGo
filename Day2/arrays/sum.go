da863d2461c06c958f091859bd1d4fee998d0302
canro91 <canro91@hotmail.com>
2020-04-03 22:27:44 -0500

    Add SumAll

--- Day2/arrays/sum.go
+++ Day2/arrays/sum.go
@@ -1,9 +1,18 @@
 package arrays
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, n := range numbers {
 		sum += n
 	}
 	return sum
+}
+
+func SumAll(arrays ...[]int) []int {
+	var sums []int
+
+	for _, v := range arrays {
+		sums = append(sums, Sum(v))
+	}
+	return sums
 }
--- Day2/arrays/sum_test.go
+++ Day2/arrays/sum_test.go
@@ -1,16 +1,29 @@
 package arrays
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T){
-	array := [5]int{ 1, 2, 3, 4, 5}
 
-	expected := 15
-	sum := Sum(array)
+	t.Run("Sum_ArrayOfAnyLength_ReturnsSum", func (t *testing.T) {
+		array := []int{ 1, 2, 3 }
+
+		expected := 6
+		sum := Sum(array)
+		
+		if expected != sum {
+			t.Errorf("Expected %d but was %d", expected, sum)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T){
+	expected := []int{ 6, 3 }
+	sum := SumAll([]int{ 1,2,3 }, []int { 1,2 })
 	
-	if expected != sum {
+	if !reflect.DeepEqual(expected, sum) {
 		t.Errorf("Expected %d but was %d", expected, sum)
-	}
+	}	
 }
