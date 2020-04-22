d210a9ef0a087017feaae5c5e1de2dbea16748ec
canro91 <canro91@hotmail.com>
2020-04-21 21:23:20 -0500

    Add select

--- Day20/select/racer.go
+++ Day20/select/racer.go
@@ -1,22 +1,37 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(urlA, urlB string) string {
-	durationA := duration(urlA)
-	durationB := duration(urlB)
+var (
+	TenSecondsTimeout = 10 * time.Second
+)
 
-	if durationA < durationB {
-		return urlA
-	}
-	return urlB
+func Racer(urlA, urlB string) (string, error) {
+	return ConfigurableRacer(urlA, urlB, TenSecondsTimeout)
 }
 
-func duration(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ConfigurableRacer(urlA, urlB string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(urlA):
+		return urlA, nil
+
+	case <-ping(urlB):
+		return urlB, nil
+
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", urlA, urlB)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
--- Day20/select/racer_test.go
+++ Day20/select/racer_test.go
@@ -8,27 +8,33 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
+	t.Run("Return url of first reponder", func(t *testing.T) {
+		slowServer := makeServer(20 * time.Millisecond)
+		fastServer := makeServer(0 * time.Millisecond)
 
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
 
-	slowUrl := slowServer.URL
-	fastUrl := fastServer.URL
+		expected := fastUrl
+		got, _ := Racer(slowUrl, fastUrl)
 
-	expected := fastUrl
-	got := Racer(slowUrl, fastUrl)
+		if expected != got {
+			t.Errorf("Expected %s, but want %s", expected, got)
+		}
+	})
 
-	if expected != got {
-		t.Errorf("Expected %s, but want %s", expected, got)
-	}
+	t.Run("Return error if urls don't response after 10 sec", func(t *testing.T) {
+		server := makeServer(20 * time.Millisecond)
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 10*time.Millisecond)
+
+		if err == nil {
+			t.Errorf("Expected error but was nil")
+		}
+	})
 }
 
 func makeServer(delay time.Duration) *httptest.Server {
