5761c3a99c763bed0d40ca1d845e59a37cb38bce
canro91 <canro91@hotmail.com>
2020-04-24 00:08:17 -0500

    Add more test cases

--- Day22/reflection/reflection.go
+++ Day22/reflection/reflection.go
@@ -4,8 +4,39 @@ import (
 	"reflect"
 )
 
-func walk(x interface{}, f func(input string)){
+func walk(x interface{}, f func(input string)) {
 	value := reflect.ValueOf(x)
-	field := value.Field(0)
-	f(field.String())
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), f)
+	}
+
+	switch value.Kind() {
+	case reflect.String:
+		f(value.String())
+	case reflect.Struct:
+		for i := 0; i < value.NumField(); i++ {
+			field := value.Field(i)
+			walkValue(field)
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < value.Len(); i++ {
+			walkValue(value.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range value.MapKeys() {
+			walkValue(value.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := value.Recv(); ok; v, ok = value.Recv() {
+			walk(v.Interface(), f)
+		}
+	case reflect.Func:
+		valFnResult := value.Call(nil)
+		for _, res := range valFnResult {
+			walk(res.Interface(), f)
+		}
+	case reflect.Ptr:
+		walkValue(value.Elem())
+	}
 }
--- Day22/reflection/reflection_test.go
+++ Day22/reflection/reflection_test.go
@@ -5,27 +5,85 @@ import (
 	"testing"
 )
 
-func TestWalk(t *testing.T){
+type Person struct {
+	Name    string
+	Profile Profile
+}
 
-	cases := []struct{
-		Name string
-		Input interface{}
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
 		ExpectedCalls []string
 	}{
 		{
 			"Single property",
-			struct{
-				name string
+			struct {
+				Name string
 			}{"Alice"},
 			[]string{"Alice"},
+		},
+		{
+			"Two properties",
+			struct {
+				Name string
+				City string
+			}{"Alice", "Wonderland"},
+			[]string{"Alice", "Wonderland"},
+		},
+		{
+			"A property isn't string",
+			struct {
+				Name string
+				Age  int
+			}{"Alice", 16},
+			[]string{"Alice"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Alice",
+				Profile{16, "Wonderland"},
+			},
+			[]string{"Alice", "Wonderland"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Alice",
+				Profile{16, "Wonderland"},
+			},
+			[]string{"Alice", "Wonderland"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{1, "Wonderland"},
+				{2, "Howards"},
+			},
+			[]string{"Wonderland", "Howards"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{1, "Wonderland"},
+				{2, "Howards"},
+			},
+			[]string{"Wonderland", "Howards"},
 		},
 	}
 
 	for _, test := range cases {
-		t.Run(test.Name, func(t *testing.T){
+		t.Run(test.Name, func(t *testing.T) {
 			var actual []string
 
-			walk(test.Input, func(input string){
+			walk(test.Input, func(input string) {
 				actual = append(actual, input)
 			})
 
@@ -33,5 +91,72 @@ func TestWalk(t *testing.T){
 				t.Errorf("Expected %s, but was %s", test.ExpectedCalls, actual)
 			}
 		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Katowice"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	t.Helper()
+
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
 	}
 }
