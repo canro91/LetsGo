cab4908d619cd4bdf616b6f837e8bb49ca800ba5
canro91 <canro91@hotmail.com>
2020-04-29 22:53:02 -0500

    Convert from roman to arabic

--- Day28/roman/roman.go
+++ Day28/roman/roman.go
@@ -5,11 +5,13 @@ import (
 )
 
 type Roman struct {
-	Value int
+	Value uint16
 	Symbol string
 }
 
-var romans = []Roman{
+type RomanNumerals []Roman
+
+var allRomans = RomanNumerals{
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -25,13 +27,63 @@ var romans = []Roman{
 	{1, "I"},
 }
 
-func ConvertToRoman(n int) string {
+func ConvertToRoman(n uint16) string {
 	var builder strings.Builder
-	for _, roman := range romans {
+	for _, roman := range allRomans {
 		for n >= roman.Value {
 			builder.WriteString(roman.Symbol)
 			n -= roman.Value
 		}
 	}
 	return builder.String()
+}
+
+type windowedRoman string
+
+func (w windowedRoman) Symbols() (symbols [][]byte) {
+	for i := 0; i < len(w); i++ {
+		symbol := w[i]
+		notAtEnd := i+1 < len(w)
+
+		if notAtEnd && isSubtractive(symbol) && allRomans.Exists(symbol, w[i+1]) {
+			symbols = append(symbols, []byte{byte(symbol), byte(w[i+1])})
+			i++
+		} else {
+			symbols = append(symbols, []byte{byte(symbol)})
+		}
+	}
+	return
+}
+
+func isSubtractive(symbol uint8) bool {
+	return symbol == 'I' || symbol == 'X' || symbol == 'C'
+}
+
+func (r RomanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+	return false
+}
+
+func (r RomanNumerals) ValueOf(symbols ...byte) uint16 {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
+func ConvertToArabic(roman string) uint16 {
+	var total uint16 = 0
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		total += allRomans.ValueOf(symbols...)
+	}
+	return total
 }
--- Day28/roman/roman_test.go
+++ Day28/roman/roman_test.go
@@ -1,45 +1,47 @@
 package roman
 
 import (
+	"testing/quick"
 	"fmt"
 	"testing"
 )
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
-		Arabic      int
-		Roman       string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{7, "VII"},
-		{8, "VIII"},
-		{9, "IX"},
-		{10, "X"},
-		{14, "XIV"},
-		{18, "XVIII"},
-		{20, "XX"},
-		{39, "XXXIX"},
-		{40, "XL"},
-		{47, "XLVII"},
-		{49, "XLIX"},
-		{50, "L"},
-		{100,"C"},
-		{90,"XC"},
-		{400,"CD"},
-		{500,"D"},
-		{900,"CM"},
-		{1000,"M"},
-		{1984,"MCMLXXXIV"},
-		{3999,"MMMCMXCIX"},
-		{2014,"MMXIV"},
-		{1006,"MVI"},
-		{798,"DCCXCVIII"},
-	}
+var cases = []struct {
+	Arabic      uint16
+	Roman       string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{7, "VII"},
+	{8, "VIII"},
+	{9, "IX"},
+	{10, "X"},
+	{14, "XIV"},
+	{18, "XVIII"},
+	{20, "XX"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+	{100,"C"},
+	{90,"XC"},
+	{400,"CD"},
+	{500,"D"},
+	{900,"CM"},
+	{1000,"M"},
+	{1984,"MCMLXXXIV"},
+	{3999,"MMMCMXCIX"},
+	{2014,"MMXIV"},
+	{1006,"MVI"},
+	{798,"DCCXCVIII"},
+}
+
+func TestConvertToRomanNumerals(t *testing.T) {
 	for _, testCase := range cases {
 		t.Run(fmt.Sprintf("%d gets converted to %q", testCase.Arabic, testCase.Roman), func(t *testing.T) {
 			got := ConvertToRoman(testCase.Arabic)
@@ -49,5 +51,34 @@ func TestRomanNumerals(t *testing.T) {
 				t.Errorf("got %q, want %q", got, want)
 			}
 		})
+	}
+}
+
+func TestConvertToArabicNumerals(t *testing.T) {
+	for _, testCase := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %d", testCase.Roman, testCase.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(testCase.Roman)
+			want := testCase.Arabic
+
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
