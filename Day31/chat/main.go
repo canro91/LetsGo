673ffea03969a93dd25eabcb60bbd167b29d55c8
canro91 <canro91@hotmail.com>
2020-06-04 21:04:53 -0500

    Broadcast message to all clients

--- Day31/chat/main.go
+++ Day31/chat/main.go
@@ -16,13 +16,82 @@ type ClientManager struct {
 	unregister chan *Client
 }
 
+func (manager *ClientManager) start() {
+	for {
+		select {
+		case connection := <-manager.register:
+			manager.clients[connection] = true
+			fmt.Println("New connection added")
+
+		case connection := <-manager.unregister:
+			if _, ok := manager.clients[connection]; ok {
+				close(connection.data)
+				delete(manager.clients, connection)
+				fmt.Println("Connection terminated")
+			}
+
+		case message := <-manager.broadcast:
+			for connection := range manager.clients {
+				select {
+				case connection.data <- message:
+				default:
+					close(connection.data)
+					delete(manager.clients, connection)
+				}
+			}
+		}
+	}
+}
+
+func (manager *ClientManager) receive(client *Client) {
+	for {
+		message := make([]byte, 4096)
+		length, err := client.socket.Read(message)
+		if err != nil {
+			manager.unregister <- client
+			client.socket.Close()
+			break
+		}
+
+		if length > 0 {
+			fmt.Println("*** Receive: " + string(message))
+			manager.broadcast <- message
+		}
+	}
+}
+
+func (manager *ClientManager) send(client *Client) {
+	defer client.socket.Close()
+	for {
+		select {
+		case message, ok := <-client.data:
+			if !ok {
+				return
+			}
+
+			client.socket.Write(message)
+		}
+	}
+}
+
 type Client struct {
 	socket net.Conn
 	data   chan []byte
 }
 
-func (c *Client) receive() {
+func (client *Client) receive() {
+	for {
+		message := make([]byte, 4096)
+		length, err := client.socket.Read(message)
+		if err != nil {
+			client.socket.Close()
+			break
+		}
 
+		if length > 0 {
+			fmt.Println("*** Receive: " + string(message))
+		}
+	}
 }
 
 func startServerMode() {
@@ -44,13 +113,15 @@ func startServerMode() {
 		if err != nil {
 			fmt.Println(err)
 		}
+
 		client := &Client{
 			socket: connection,
 			data:   make(chan []byte),
 		}
 		manager.register <- client
+		go manager.receive(client)
+		go manager.send(client)
 	}
-
 }
 
 func startClientMode() {
@@ -68,16 +139,6 @@ func startClientMode() {
 		reader := bufio.NewReader(os.Stdin)
 		message, _ := reader.ReadString('\n')
 		connection.Write([]byte(strings.TrimRight(message, "\n")))
-	}
-}
-
-func (manager *ClientManager) start() {
-	for {
-		select {
-		case connection := <-manager.register:
-			manager.clients[connection] = true
-			fmt.Println("New connection added")
-		}
 	}
 }
 
